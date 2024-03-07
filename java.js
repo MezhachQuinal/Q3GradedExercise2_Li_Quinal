4da05f461aa900da063687c5d2c5e4afd19f6e0d
MezhachQuinal <143673443+MezhachQuinal@users.noreply.github.com>
2024-03-07 22:05:40 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+function OpenPage() {
+    alert("You will be redirected to another page.");
+    window.open("second.html");
+}
+function change(){
+    document.getElementById("image").src="images/Julius_cool.jpg";
+}
+function back(){
+    document.getElementById("image").src="images/Julius.jpg";
+}
